c9517155283cfbb27c6da22b401900ab11d48b93
Marcus Meissner <meissner@suse.de>
2022-09-12 10:13:24 +0200

    use references better to make it clearer

--- mksbom.pl
+++ mksbom.pl
@@ -4,7 +4,7 @@ use POSIX qw/strftime setlocale LC_TIME/;
 use JSON;
 use Getopt::Long;
 
-my $url = "";
+my $url = "https://github.com/msmeissn/rpm-list-to-sbom";
 my $name = "unset";
 
 GetOptions ( "url=s" => \$url, "name=s"   => \$name)
@@ -13,32 +13,24 @@ GetOptions ( "url=s" => \$url, "name=s"   => \$name)
 
 my @output = ();
 
-my %map = (
-);
-
-$map{"spdxVersion"} = "SPDX-2.2";
-$map{"dataLicense"} = "CC-BY-4.0";
-$map{"SPDXID"} = "SPDXRef-DOCUMENT";
-$map{"name"} = $name;
-
-my %creationinfo = ();
-# foreach creator...
-	my @creators = ();
-		my %creator = ();
-		$creator{"Tool"} = $0;
-		push @creators,\%creator;
-$creationinfo{"Creators"} = \@creators;
 my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
 my $curtime = POSIX::strftime("%Y-%m-%dT%H:%M:%S",$sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst );
-$creationinfo{"Created"} = $curtime;
 
-$map{"CreationInfo"} = \%creationinfo;
-
-$map{"documentNamespace"} = "https://ftp.suse.com/pub/projects/security/spdx/";	# FIXME
+my %map = (
+	"spdxVersion"		=> "SPDX-2.2",
+	"dataLicense"		=> "CC-BY-4.0",
+	"SPDXID"		=> "SPDXRef-DOCUMENT",
+	"name"			=> $name,
+	"documentNamespace"	=> "https://ftp.suse.com/pub/projects/security/spdx/",	# FIXME
+	"CreationInfo" 		=> {
+		"Creators"	=> [ {"Tool" => $0, } ],
+		"Created"	=> $curtime,
+	},
+);
 
 my @describes = ("outputfile");				# FIXME
-
 $map{"documentDescribes"} = \@describes;
+
 
 my @packages = ();
 
@@ -56,46 +48,40 @@ foreach my $rpm (@rpms) {
 	my $license = `rpm -qp --qf '%{LICENSE}\n' $rpm`;
 	chomp($license);
 
-	my %package = ();
-	$package{"packageName"} = $name;		# FIXME ... full version and arch?
-	$package{"packageVersion"} = $version;
-	$package{"packageFilename"} = $basename;
-	$package{"SPDXID"} = "SPDXRef-$basename";
-	#$package{"downloadLocation"} = "$url$rpm";
-	$package{"filesAnalyzed"} = "false";
-
-	$package{"packageLicenseConcluded"}	= $license;
-	$package{"packageLicenseDeclared"} 	= $license;
-	$package{"packageCopyrightText"}	= $license;
 	my $url = `rpm -qp --qf '%{URL}\n' $rpm`;
 	chomp($url);
-	if ($url ne "(none)") {
-		$package{"homepage"}			= $url;
-	}
 	my $sourceinfo = `rpm -qp --qf '%{SOURCERPM}\n' $rpm`;
 	chomp($sourceinfo);
-	$package{"sourceInfo"}			= $sourceinfo;
 
 	my $description = `rpm -qp --qf '%{DESCRIPTION}\n' $rpm`;
 	chomp($description);
-	$package{"description"}			= $description;
 	my $summary = `rpm -qp --qf '%{SUMMARY}\n' $rpm`;
 	chomp($summary);
-	$package{"summary"}			= $summary;
 
-		#"checksum": { "algorithm": "SHA1", "checksumValue": "14ff98203c3ddd2bd4803c00b5225d2551ca603c" },
-		my @checksums = ();
-		my %checksum = ();
-		my $sha2 = `sha256sum $rpm`;
-		chomp($sha2);
-		$sha2 =~ s/ .*//;
-		$checksum{'algorithm'}		= "SHA256";
-		$checksum{'checksumValue'}	= $sha2;
+	#"checksum": { "algorithm": "SHA1", "checksumValue": "14ff98203c3ddd2bd4803c00b5225d2551ca603c" },
+	my $sha2 = `sha256sum $rpm`;
+	chomp($sha2);
+	$sha2 =~ s/ .*//;
 
-		@checksums = ( \%checksum );
-
-	$package{'checksum'}=\@checksums;
-
+	my %package = (
+		"description"			=> $description,
+		"summary"			=> $summary,
+		"homepage"			=> $url,
+		"packageName"			=> $name, # FIXME ... full version and arch?
+		"packageVersion"		=> $version,
+		"sourceInfo"			=> $sourceinfo,
+		"packageFilename"		=> $basename,
+		"SPDXID" 			=> "SPDXRef-$basename",
+		#"downloadLocation"		=> "$url$rpm",	# FIXME
+		"filesAnalyzed" 		=> "false",
+		"packageLicenseConcluded"	=> $license,
+		"packageLicenseDeclared" 	=> $license,
+		"packageCopyrightText"		=> $license,
+		"checksum"			=> [ {
+			"algorithm" 	=> "SHA256",
+			"checksumValue"	=> $sha2,
+		} ],
+	);
 	push @packages,\%package;
 }
 
